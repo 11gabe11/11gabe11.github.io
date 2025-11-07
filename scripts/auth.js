04666083df605c8c633838f0e092593192cab5ee
11gabe11 <90136848+11gabe11@users.noreply.github.com>
2025-11-07 14:31:52 +1100

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,7 +1,7 @@
 // Initialize Supabase client
 const supabase = createClient(
-  "https://YOURPROJECT.supabase.co",
-  "YOUR-PUBLIC-ANON-KEY"
+  "https://uzzcupnudyhvzdtwpjiw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6emN1cG51ZHlodnpkdHdwaml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0ODI3MTUsImV4cCI6MjA3ODA1ODcxNX0.ez6WS7tUemjAKr8fCyM2_WH4fNg6aoo7UoudFqmFd8E"
 );
 
 // signup example
